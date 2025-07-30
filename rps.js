a3d80c4677a88ae3638852e4821c9ca569a8f37b
Joao Pedro <joao.pedrosousa5@gmail.com>
2025-07-29 22:18:07 -0300

    Functions created

---
+++ rps.js
@@ -0,0 +1,25 @@
+function getComputerChoice(){
+  Math.floor(Math.random() * 3);
+  //randomly return string values r p or s
+  return
+ 
+}
+
+function getHumanChoice(){
+ //retorna escolhas validas baseado no input do user
+ prompt();
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(computerChoice, humanChoice){
+
+    /*if(humanChoice = rock && computerChoice = paper){
+        humanScore += 1;    
+    } else if (humanxhoixe = paper ...)*/
+}
+
+function playGame(){
+    playRound();
+}
