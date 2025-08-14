6cd63efba602f8974287066aea7b1502c37a08a3
João Pedro Santana <joao.pedrosousa5@gmail.com>
2025-08-14 15:14:43 -0300

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,23 +1,55 @@
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
-    return computerChoice = "rock";
+    return "rock";
   } else if (randomNumber === 1) {
-    return computerChoice = "paper";
+    return "paper";
   } else {
-    return computerChoice = "scissors";
+    return "scissors";
   }
 }
 
-function getHumanChoice(){
-    const choose = prompt("Choose: rock, paper or scissors");
-    if(choose == "rock"){
-        return "rock"
-    } else if(choose == "paper"){
-        return "paper"
-    } else if (choose == "scissors"){
-        return "scissors"
-    }
-
+function getHumanChoice() {
+  const choose = prompt("Choose: rock, paper or scissors").toLowerCase();
+  if (choose == "rock") {
+    return "rock";
+  } else if (choose == "paper") {
+    return "paper";
+  } else if (choose == "scissors") {
+    return "scissors";
+  }
 }
-console.log(getHumanChoice())
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+      console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+
+  if (humanScore > computerScore) {
+    console.log("Human Wins!");
+  } else {
+    console.log("Computer Wins!");
+  }
+}
+
+playGame();
